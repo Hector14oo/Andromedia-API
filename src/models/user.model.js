e68531c05594f4967e6610cf049f99cca4f6df41
Hector14oo <devtor140017@gmail.com>
2023-09-19 11:39:13 -0500

    :card_file_box: User schema/model added

---
+++ src/models/user.model.js
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+
+export const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+});
+
+export default model('User', userSchema);
