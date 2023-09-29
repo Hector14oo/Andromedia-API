40f225bd347bda8d1f97617d92dce1773392763c
Hector14oo <devtor140017@gmail.com>
2023-09-29 17:13:10 -0500

    :recycle: added alternative return

--- src/libs/filters/details/runtimeCalculator.js
+++ src/libs/filters/details/runtimeCalculator.js
@@ -2,5 +2,6 @@ export function runtimeCalculator(totalMinutes) {
   const hours = Math.floor(totalMinutes / 60);
   const minutes = totalMinutes % 60;
 
+  if (!hours) return `${minutes}m`;
   return `${hours}h ${minutes}m`;
 }
