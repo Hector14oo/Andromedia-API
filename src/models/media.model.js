c32757b2456f0dadc35f1b3bc4257a62d81d0f62
Hector14oo <devtor140017@gmail.com>
2023-10-02 16:22:57 -0500

    :recycle: Refactor, improved code

--- src/models/media.model.js
+++ src/models/media.model.js
@@ -1,49 +1,35 @@
-import { options } from '../config.js';
+import { fetchAPI } from '../libs/fetch.js';
+import { SEARCH, TRENDING, MEDIADETAILS, REVIEWS, MOVIECREDITS, SEASONDETAILS } from '../libs/constants/endpoints.js';
 import { getDetails, getDetailsFromList, getSeasonDetails } from '../libs/filters/index.js';
-import { urlConstructor } from '../libs/urlConstructor.js';
 
 export class Media {
-  static getSearch = async ({ search, lang = 'en-US', page = 1 }) => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/search/multi?query=${search}&language=${lang}&page=${page}`,
-      options
-    );
-    const data = await response.json(response);
+  static getSearch = async ({ search, page, lang }) => {
+    const data = await fetchAPI(SEARCH({ search, page, lang }));
     return getDetailsFromList(data, lang);
   };
 
-  static getTrending = async ({timeWindow = 'week', lang = 'en-US', page = 1}) => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/trending/all/${timeWindow}?language=${lang}&page=${page}`,
-      options
-    );
-    const data = await response.json();
+  static getTrending = async ({ timeWindow, page, lang }) => {
+    const data = await fetchAPI(TRENDING({ timeWindow, page, lang }));
     return getDetailsFromList(data, lang);
   };
 
-  static getMediaDetails = async ({ lang, id, mediaType }) => {
-    const response = await fetch(urlConstructor({ lang, id, mediaType }), options);
-    const data = await response.json();
+  static getMediaDetails = async ({ mediaType, id, lang }) => {
+    const data = await fetchAPI(MEDIADETAILS({ mediaType, id, lang }));
 
-    if(mediaType !== 'people'){
-      const reviewsRes = await fetch(`https://api.themoviedb.org/3/${mediaType}/${id}/reviews`, options);
-      const reviewsData = await reviewsRes.json();
+    if (mediaType !== 'people') {
+      const reviewsData = await fetchAPI(REVIEWS({ mediaType, id }));
       data.reviews = reviewsData;
     }
-
-    if(mediaType === 'movie'){
-      const creditsRes = await fetch(`https://api.themoviedb.org/3/movie/${id}/credits`, options);
-      const creditsData = await creditsRes.json();
+    if (mediaType === 'movie') {
+      const creditsData = await fetchAPI(MOVIECREDITS({ id }));
       data.credits = creditsData;
     }
 
-    return getDetails({data, mediaType, lang});
+    return getDetails({ data, mediaType, lang });
   };
 
-  static getSeasonDetails = async ({lang = 'en-US', id, seasonNumber}) => {
-    const response = await fetch(`https://api.themoviedb.org/3/tv/${id}/season/${seasonNumber}?lang=${lang}`, options);
-    const data = await response.json();
-
-    return getSeasonDetails(data)
-  }
+  static getSeasonDetails = async ({ id, seasonNumber, lang }) => {
+    const data = await fetchAPI(SEASONDETAILS({ id, seasonNumber, lang }));
+    return getSeasonDetails(data);
+  };
 }
