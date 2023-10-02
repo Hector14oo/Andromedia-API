a13db1c348d6bb48ae3f6320455a2799db9efe1c
Hector14oo <devtor140017@gmail.com>
2023-10-02 17:20:03 -0500

    :recycle: Refactor, improved code

--- src/libs/filters/details/movie.js
+++ src/libs/filters/details/movie.js
@@ -1,70 +1,42 @@
 import { getGenres } from '../../constants/genres.js';
-import { runtimeCalculator } from './runtimeCalculator.js';
-import { reviewsFilter } from './reviews.js';
-import { imagesObj, imagesFromList, videosFromList } from './assets.js';
+import { imagesObj, imagesFromList, videosFromList, reviewsFilter, runtimeCalculator } from './extras/index.js';
 import { personBasicDetails } from './person.js';
 
 export const movieBasicDetails = (movie, lang) => {
-  const {
-    id,
-    title,
-    adult,
-    genre_ids,
-    tagline,
-    overview,
-    poster_path,
-    backdrop_path,
-    release_date,
-    vote_average,
-    media_type
-  } = movie;
+  const { id, title, adult, genre_ids, tagline, overview, poster_path, backdrop_path, release_date, vote_average, media_type } = movie;
 
   if (!genre_ids.lenght && !overview && !release_date) return null;
 
-  const genres = getGenres(genre_ids, 'movie');
+  const images = imagesObj({ poster_path, backdrop_path });
+  const url = `${globalThis.process.env}/api/media/movie/${id}${lang || ''}`
 
   return {
     id,
     title,
     adult,
-    genres,
+    genres: getGenres(genre_ids, 'movie'),
     tagline,
     overview,
-    ...imagesObj({ poster_path, backdrop_path }),
+    ...images,
     year: release_date,
     votes: vote_average,
     type: media_type,
-    url: `http://localhost:1234/api/media/movie/${id}${lang ? '?lang=' + lang : ''}`,
+    url,
   };
 };
 
-export const movieExtraDetails = (movie, reviews, credits, lang) => {
-  const {
-    id,
-    title,
-    adult,
-    genres,
-    runtime,
-    tagline,
-    overview,
-    images,
-    videos,
-    release_date,
-    vote_average,
-    belongs_to_collection,
-  } = movie;
+export const movieExtraDetails = ({ data, reviews, credits, lang }) => {
+  const { id, title, adult, genres, runtime, tagline, overview, images, videos, release_date, vote_average, belongs_to_collection } = data;
 
-  const collection = belongs_to_collection
-    ? {
-        id: belongs_to_collection.id,
-        name: belongs_to_collection.name,
-        ...imagesObj({
-          poster_path: belongs_to_collection.poster_path,
-          backdrop_path: belongs_to_collection.backdrop_path,
-        }),
-      }
-    : null;
-
+  const collection = belongs_to_collection && {
+    id: belongs_to_collection.id,
+    name: belongs_to_collection.name,
+    ...imagesObj({
+      poster_path: belongs_to_collection.poster_path,
+      backdrop_path: belongs_to_collection.backdrop_path,
+    }),
+  }
+    
   return {
     id,
     title,
@@ -81,10 +53,10 @@ export const movieExtraDetails = (movie, reviews, credits, lang) => {
     videos: videosFromList(videos.results),
     date: release_date,
     votes: vote_average,
-    collection,
+    collection: collection || null,
     credits: {
-      cast: credits.cast.map(({character, ...data}) => ({character, ...personBasicDetails(data, lang)})),
-      crew: credits.crew.map(({department, job, ...data}) => ({department, job, ...personBasicDetails(data, lang)}))
-    }
+      cast: credits.cast.map(({ character, ...data }) => ({ character, ...personBasicDetails(data, lang) })),
+      crew: credits.crew.map(({ department, job, ...data }) => ({ department, job, ...personBasicDetails(data, lang) })),
+    },
   };
 };
