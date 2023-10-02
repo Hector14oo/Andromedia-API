052bc81128f79663851b977c182d5b6211e7f5f6
Hector14oo <devtor140017@gmail.com>
2023-10-02 18:12:16 -0500

    :recycle: Improved code

--- src/libs/filters/details/person.js
+++ src/libs/filters/details/person.js
@@ -1,53 +1,32 @@
-import { imagesFromList } from './assets.js';
+import { imagesFromList } from './extras/assets.js';
 import { movieBasicDetails } from './movie.js';
 import { genders } from '../../constants/genders.js';
 
-export const personBasicDetails = (person, lang) => {
-  const { adult, id, name, profile_path, media_type } = person;
+export const personBasicDetails = ({ data, lang }) => {
+  const { adult, id, name, profile_path, media_type } = data;
 
   return {
     adult,
     id,
     name,
-    image: {
+    image: profile_path ? {
       light: `https://image.tmdb.org/t/p/w185${profile_path}`,
       original: `https://image.tmdb.org/t/p/original${profile_path}`,
-    },
+    } : null,
     type: media_type,
-    url: `http://localhost:1234/api/media/person/${id}${lang ? '?lang=' + lang : ''}`,
+    url: `${globalThis.process.env.HOST}/api/media/person/${id}?lang=${lang}`,
   };
 };
 
-export const personExtraDetails = (person, lang) => {
-  const {
-    adult,
-    id,
-    name,
-    gender,
-    place_of_birth,
-    birthday,
-    deathday,
-    biography,
-    known_for_department,
-    images,
-    combined_credits,
-  } = person;
+export const personExtraDetails = ({ data, lang }) => {
+  const { adult, id, name, gender, place_of_birth, birthday, deathday, biography, known_for_department, images, combined_credits } = data;
 
   const appearences = () => {
     const { cast, crew } = combined_credits;
 
     return {
-      cast: cast.map(({ credit_id, character, ...movie }) => ({
-        creditId: credit_id,
-        character,
-        ...movieBasicDetails(movie, lang),
-      })),
-      crew: crew.map(({ credit_id, department, job, ...movie }) => ({
-        creditId: credit_id,
-        department,
-        job,
-        ...movieBasicDetails(movie),
-      })),
+      cast: cast.map(({ credit_id, character, ...movie }) => ({ creditId: credit_id, character, ...movieBasicDetails(movie, lang) })),
+      crew: crew.map(({ credit_id, department, job, ...movie }) => ({ creditId: credit_id, department, job, ...movieBasicDetails(movie) })),
     };
   };
 
--- src/libs/filters/details/runtimeCalculator.js
+++
@@ -1,7 +0,0 @@
-export function runtimeCalculator(totalMinutes) {
-  const hours = Math.floor(totalMinutes / 60);
-  const minutes = totalMinutes % 60;
-
-  if (!hours) return `${minutes}m`;
-  return `${hours}h ${minutes}m`;
-}
