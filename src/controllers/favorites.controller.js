8a4c4c7ac5f0dc8f44d2fa380b963d4d41e3a783
Hector14oo <devtor14@gmail.com>
2023-10-07 18:54:36 -0500

    :recycle: Moved declarations to the trycatch block

--- src/controllers/favorites.controller.js
+++ src/controllers/favorites.controller.js
@@ -5,6 +5,7 @@ export class FavoritesController {
     try {
       const { id } = req.user;
       const data = await FavoritesModel.getFavorites({ userId: id });
+      
       res.json(200, data);
     } catch (error) {
       res.json(400, error.message)
@@ -12,11 +13,11 @@ export class FavoritesController {
   };
 
   static addFavorite = async (req, res) => {
-    const { movieId, title, poster, url } = req.body;
-    const { id } = req.user;
-    
     try {
+      const { movieId, title, poster, url } = req.body;
+      const { id } = req.user;
       const data = await FavoritesModel.addFavorites({ movieId, title, poster, url, userId: id });
+
       res.json(201, data);
     } catch (error) {
       res.json(400, error.message)
@@ -24,11 +25,12 @@ export class FavoritesController {
   };
   
   static removeFavorite = async (req, res) => {
-    const { id } = req.params;
-    
     try {
+      const { id } = req.params;
       const data = await FavoritesModel.removeFavorite({ favId: id });
+      
       if(!data) throw new Error({ message: 'File not found' })
+
       res.json(204, { message: 'Favorite deleted successfully' });
     } catch (error) {
       if(error.message === 'File not found') return res.json(404, error.message)
