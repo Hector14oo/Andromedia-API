37eed834ce40c022812310e96b248d553cc0dc44
Hector14oo <devtor14@gmail.com>
2023-10-06 21:15:42 -0500

    :recycle: Added Favorites Route

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { app } from './app.js';
 import { mediaRouter } from './routes/media.routes.js';
 import { authRouter } from './routes/auth.routes.js';
+import { favoritesRouter } from './routes/favorites.routes.js';
 import { connectDB } from './db.js';
 
 connectDB();
 app.use('/api/media', mediaRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/favorites', favoritesRouter);
 
 const PORT = globalThis.process.env.PORT;
 
