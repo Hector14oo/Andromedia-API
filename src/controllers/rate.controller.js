f0659f56403707eb07150b31b5f9068c991239c1
Hector14oo <devtor14@gmail.com>
2023-10-08 00:07:20 -0500

    :sparkles: Added model and controller for Rate routes

---
+++ src/controllers/rate.controller.js
@@ -0,0 +1,47 @@
+import { RateModel } from '../models/rate.model.js';
+
+export class RateController {
+  static getRateList = async (req, res) => {
+    try {
+      const { guestId } = req.params;
+      
+      const data = await RateModel.getRate({ guestId })
+
+      if (data.success === false) return res.status(data.status).json(data);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+  static rateMovie = async (req, res) => {
+    try {
+      const { body } = req;
+      const { guestId, id } = req.params;
+
+      const data = await RateModel.addMovieRate({ id, guestId, body });
+
+      if (data.success === false) return res.status(data.status).json(data);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+  static rateTvShow = async (req, res) => {
+    const { body } = req;
+
+    try {
+      const { guestId, id } = req.params;
+      const data = await RateModel.addTvShowRate({ id, guestId, body });
+
+      if (data.success === false) return res.status(data.status).json(data);
+
+      res.json(data);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+}
