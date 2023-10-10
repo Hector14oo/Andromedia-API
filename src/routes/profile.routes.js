622e378bcc2efe1cdab3766d4805f9b8b999ad56
Hector14oo <devtor14@gmail.com>
2023-10-09 21:14:05 -0500

    :recycle: Modified some routes

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { validateJWT } from '../middlewares/validateJWT.js';
+import { ProfileController } from '../controllers/profile.controller.js';
 
 export const profileRouter = Router();
 
 profileRouter.get('/', validateJWT, ProfileController.getProfile);
 
-profileRouter.patch('/update', validateJWT, ProfileController.updateProfile);
+profileRouter.put('/update', validateJWT, ProfileController.updateProfile);
 
 profileRouter.delete('/delete', validateJWT, ProfileController.deleteProfile);
