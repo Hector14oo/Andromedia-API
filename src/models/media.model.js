6085d01cd9a8f21602f3f4c90a526d00a9f34c5d
Hector14oo <devtor14@gmail.com>
2023-10-10 17:47:19 -0500

    :recycle: Update imports

--- src/models/media.model.js
+++ src/models/media.model.js
@@ -1,26 +1,26 @@
 import { fetchAPI } from '../libs/fetchAPI.js';
 import { SEARCH, TRENDING, MEDIADETAILS, REVIEWS, MOVIECREDITS, SEASONDETAILS } from '../libs/constants/endpoints.js';
 import { getDetails, getDetailsFromList, getSeasonDetails } from '../libs/filters/index.js';
-import { createError } from '../libs/constants/errors.js';
+import { formatResponse } from '../libs/constants/errors.js';
 
 export class Media {
   static getSearch = async ({ search, page, lang }) => {
     const data = await fetchAPI(SEARCH({ search, page, lang }));
-    if(data.success === false) return createError(data);
+    if(data.success === false) return formatResponse(data);
 
     return getDetailsFromList({ data, lang });
   };
 
   static getTrending = async ({ timeWindow, page, lang }) => {
     const data = await fetchAPI(TRENDING({ timeWindow, page, lang }));
-    if(data.success === false) return createError(data);
+    if(data.success === false) return formatResponse(data);
 
     return getDetailsFromList({ data, lang });
   };
 
   static getMediaDetails = async ({ mediaType, id, lang }) => {
     const data = await fetchAPI(MEDIADETAILS({ mediaType, id, lang }));
-    if(data.success === false) return createError(data);
+    if(data.success === false) return formatResponse(data);
 
     if (mediaType !== 'people') {
       const reviewsData = await fetchAPI(REVIEWS({ mediaType, id }));
@@ -36,7 +36,7 @@ export class Media {
 
   static getSeasonDetails = async ({ id, seasonNumber, lang }) => {
     const data = await fetchAPI(SEASONDETAILS({ id, seasonNumber, lang }));
-    if(data.success === false) return createError(data);
+    if(data.success === false) return formatResponse(data);
 
     return getSeasonDetails(data);
   };
