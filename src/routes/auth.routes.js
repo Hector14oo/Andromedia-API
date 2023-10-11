b5e98662dfeb4505bcc6eb60c99efdd0e430b98f
Hector14oo <devtor14@gmail.com>
2023-10-11 17:20:31 -0500

    :safety_vest: Added new middleware for validations

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { AuthController } from '../controllers/auth.controller.js';
+import { authValidator } from '../schemas/auth.schema.js'
+import { schemaValidator } from '../middlewares/schemaValidator.js';
 
 export const authRouter = Router();
 
-authRouter.post('/register', AuthController.Register);
+authRouter.post('/register', schemaValidator(authValidator), AuthController.Register);
 
-authRouter.post('/login', AuthController.Login);
+authRouter.post('/login', schemaValidator(authValidator), AuthController.Login);
 
 authRouter.post('/logout', AuthController.Logout);
