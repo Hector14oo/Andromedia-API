834f04f58614a375977d284094f3c1978734c70d
Hector14oo <devtor14@gmail.com>
2023-10-12 10:09:06 -0500

    :recycle: Improved error message

--- src/middlewares/schemaValidator.js
+++ src/middlewares/schemaValidator.js
@@ -1,8 +1,9 @@
 export const schemaValidator = (schema) => (req, res, next) => {
   try {
+    console.log(req.body)
     schema.parse(req.body);
     next();
   } catch (error) {
-    return res.status(400).json({ status: 'Failure', errors: error.issues.map((err) => ({ field: err.path[0], message: err.message }))});
+    return res.status(400).json({ success: false, errors: error.issues.map((err) => ({ field: err.path.join(" > "), message: err.message }))});
   }
 };
