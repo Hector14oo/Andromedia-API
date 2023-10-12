8a9125285ed9b734b200f461eeaaf13b8e4fc988
Hector14oo <devtor14@gmail.com>
2023-10-12 09:06:32 -0500

    :safety_vest: added updateValidator in user.schema and implement in profile routes

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -22,3 +22,12 @@ export const authValidator = z.object({
   email: z.string({ required_error: 'Email is required' }).email(),
   password: z.string({ required_error: 'The password is required' }).min(6).max(20),
 })
+
+export const updateValidator = z.object({
+  username: z.string().email().optional(),
+  email: z.string().email().optional(),
+  password: z.string().min(6).max(20).optional(),
+  fullname: z.string().optional(),
+  gender: z.string(z.enum(['male, female, other'])).optional(),
+  picture: z.string().url().optional(),
+});
