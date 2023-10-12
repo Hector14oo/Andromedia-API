9c508e09fd38f0cad2c96521dd9d4754406fd6c7
Hector14oo <devtor14@gmail.com>
2023-10-12 12:31:49 -0500

    :recycle: Added a filter

--- src/controllers/favorites.controller.js
+++ src/controllers/favorites.controller.js
@@ -1,12 +1,24 @@
 import { FavoritesModel } from "../models/favorites.model.js";
 
+const filterFavorite = (favorite) => {
+  return {
+    id: favorite._id,
+    movieId: favorite.movieId,
+    title: favorite.title,
+    overview: favorite.overview,
+    poster: favorite.poster,
+    url: favorite.url,
+    userId: favorite.userId,
+  };
+}
+
 export class FavoritesController {
   static getFavorites = async (req, res) => {
     try {
       const { id } = req.user;
       const data = await FavoritesModel.getFavorites({ userId: id });
       
-      res.status(200).json({ id: data._id, movieId, title, overview, poster, url, userId: data.userId });
+      res.status(200).json(data.map(favorite => filterFavorite(favorite)));
     } catch (error) {
       res.status(500).json({ success: false, message: error.message })
     }
@@ -18,7 +30,7 @@ export class FavoritesController {
       const { id } = req.user;
       const data = await FavoritesModel.addFavorites({ movieId, title, overview, poster, url, userId: id });
 
-      res.status(201).json({ id: data._id, movieId, title, overview, poster, url, userId: data.userId });
+      res.status(201).json(filterFavorite(data));
     } catch (error) {
       if(error.message === 'Favorite already exists') return res.status(409).json({ succces: false, message: error.message });
       res.status(500).json({ success: false, message: error.message })
